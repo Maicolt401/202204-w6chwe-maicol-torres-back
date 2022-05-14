11432a057c8796762d0e1e0faa984f8dc60ae702
maicol torres <maicolt401@gmail.com>
2022-05-15 01:07:13 +0200

    elaborte api un mongo whit atlas and create routers

---
+++ server/controllers/robotControllers.test.js
@@ -0,0 +1,38 @@
+const mockRobots = require("../mocks/robots");
+const getRobots = require("./robotsControllers");
+
+jest.mock("../../db/models/Robot", () => ({
+  ...jest.requireActual("../../db/models/Robot"),
+  find: jest.fn().mockResolvedValue(mockRobots),
+}));
+
+const res = {
+  status: jest.fn().mockReturnThis(),
+  json: jest.fn(),
+};
+describe(" Given a getRobots funtion", () => {
+  describe(" Whenit`s invoked with a response", () => {
+    test("hen it should call the response method status with 200", async () => {
+      const expectStatus = 200;
+
+      await getRobots(null, res);
+
+      expect(res.status).toHaveBeenCalledWith(expectStatus);
+    });
+  });
+  test("Then it should call the method json with a list to robots", () => {
+    const expetecRobots = [
+      {
+        id: 1,
+        name: "valdimir",
+        yearCreation: 1998,
+        energy: "98%",
+        image:
+          "https://d3lqdljps13i2n.cloudfront.net/recursos/109/3518975/imagen_1_1518624548.jpg",
+      },
+    ];
+    getRobots(null, res);
+
+    expect(res.json).toHaveBeenCalledWith({ robots: expetecRobots });
+  });
+});
---
+++ server/mocks/robots.js
@@ -0,0 +1,11 @@
+const mockRobots = [
+  {
+    id: 1,
+    name: "valdimir",
+    yearCreation: 1998,
+    energy: "98%",
+    image:
+      "https://d3lqdljps13i2n.cloudfront.net/recursos/109/3518975/imagen_1_1518624548.jpg",
+  },
+];
+module.exports = mockRobots;
