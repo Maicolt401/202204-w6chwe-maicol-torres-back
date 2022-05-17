ed6ff748a89da7d1cbcffc2023d83988dd45afb6
maicol torres <maicolt401@gmail.com>
2022-05-17 03:15:37 +0200

    delete auth for fixed 500

--- server/index.js
+++ server/index.js
@@ -3,14 +3,13 @@ const cors = require("cors");
 const morgan = require("morgan");
 const { notFoundError, generalError } = require("./middlewares/error");
 const robotsRouter = require("./router/robotsRouter");
-const auth = require("./middlewares/auth");
 
 const app = express();
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
-app.use("/robots", auth, robotsRouter);
+app.use("/robots", robotsRouter);
 
 app.use(notFoundError);
 app.use(generalError);
